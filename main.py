449a9529c9405e7b77791dd03496e33073e2df12
sunderam-palavesam <53272631+sunderam-palavesam@users.noreply.github.com>
2022-12-22 12:20:13 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ Author        : Sunderam Palavesam
 Date Created  : 17 Dec 2022
 =============================================================================
 Description   : Plot the cities based on lat and lon from input csv
-                Developed as part of course work AC
+                Developed as part of course work AC50002
                 Developed as part of Python assignment Msc course Work
                 Dundee University
 credits       : https://towardsdatascience.com/easy-steps-to-plot-geographic-data-on-a-map-python-11217859a2db
 Revisions
 =============================================================================
-17122022   2474761 AC50002  initial version                               1.0
+23122022   2474761 AC50002  initial version                               1.0
 =============================================================================
 """
 # pre-requisite - 
@@ -22,22 +22,33 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('C:\Sunderam\Masters\ProgLang\pythonassignment2\GrowLocations.csv')
+#read csv and laod to a pandas dataframe
+df = pd.read_csv("C:\SSP\masters\ProgLang\wip\GrowLocations.csv")
 
-df.head()
+#data cleansing , remove dupes and extract latitudes and longitudes
+myCols =  ['Latitude','Longitude']
+df1 = df[myCols].drop_duplicates(subset=None,keep="first",inplace=False)
 
-print(df)
+#rename columns as its incorrect
+df1.rename(columns={'Latitude':'Longitude','Longitude':'Latitude'},inplace=True)
 
+#print the final cleansed result for verification
+print(df1)
+
+#define the bounding box for the map
 BBox = (-10.592,   1.6848,  50.681, 57.985)
 
-ruh_m = plt.imread('C:\Sunderam\Masters\ProgLang\pythonassignment2\map7.png')
+#load the map image
+myMap = plt.imread('C:\SSP\masters\ProgLang\wip\map7.png')
 
-fig, ax = plt.subplots(figsize = (8,7))
-ax.scatter(df.Longitude, df.Latitude, c='b')
+#create a figure and set of subplot
+fig, ax = plt.subplots(figsize = (8,10))
+ax.scatter(df1.Longitude, df1.Latitude, c='b')
 ax.set_title('Plotting Grow Data ')
 ax.set_xlim(BBox[0],BBox[1])
 ax.set_ylim(BBox[2],BBox[3])
-ax.imshow(ruh_m, zorder=0, extent = BBox, aspect= 'equal')
+ax.imshow(myMap, zorder=0, extent = BBox, aspect= 'equal')
+#show the plotted map
 plt.show()
 
 
